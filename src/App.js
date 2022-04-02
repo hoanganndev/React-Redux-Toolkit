d57f8385daf5e6d1ea729d68a083f9c4075044ac
NguyenHoangAnIT <anbest2k1@gmail.com>
2022-04-03 00:12:44 +0700

    Fetching Data Not Use Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container, Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./redux/slices/counterSlice";
+
 function App() {
     const dispatch = useDispatch();
     const count = useSelector(state => state.counter.value);
+    const [listUsers, setListUsers] = useState([]);
+    const fetchAllUsers = async () => {
+        let res = await axios.get("http://localhost:8080/users/all");
+        console.log(">>> check data:", res.data);
+        setListUsers(res.data ? res.data : []);
+    };
+    useEffect(() => {
+        fetchAllUsers();
+    }, []);
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Init project redux toolkit</h1>
-                <div className="btn-actions">
-                    <button onClick={() => dispatch(increment())}>
-                        Increase
-                    </button>
-                    <button onClick={() => dispatch(decrement())}>
-                        Decrease
-                    </button>
-                </div>
-                <br />
-                <div>Count : {count}</div>
+                <Container>
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Email</th>
+                                <th>Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {listUsers &&
+                                listUsers.length > 0 &&
+                                listUsers.map((user, index) => {
+                                    return (
+                                        <tr key={`user-${index}`}>
+                                            <td>{index + 1}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.username}</td>
+                                        </tr>
+                                    );
+                                })}
+                        </tbody>
+                    </Table>
+                </Container>
             </header>
         </div>
     );
